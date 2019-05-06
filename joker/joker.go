4d5131f8f0a830576718daa87f0a6846b73a84dd
Jens Kuehnel <JensKuehnel@users.noreply.github.com>
2019-05-06 23:33:14 +0200

    add joker DNS support

---
+++ joker/joker.go
@@ -0,0 +1,27 @@
+// Package joker adapts the lego Joker DNS
+// provider for Caddy. Importing this package plugs it in.
+package joker
+
+import (
+	"errors"
+
+	"github.com/go-acme/lego/providers/dns/joker"
+	"github.com/mholt/caddy/caddytls"
+)
+
+func init() {
+	caddytls.RegisterDNSProvider("joker", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new Joker DNS challenge provider.
+// The credentials are detected automatically; see underlying
+// package docs for details:
+// https://godoc.org/github.com/go-acme/lego/providers/dns/joker
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+	switch len(credentials) {
+	case 0:
+		return joker.NewDNSProvider()
+	default:
+		return nil, errors.New("invalid credentials length")
+	}
+}
